0952615b99a62559336031541267df6afbde0c4d
Blake Hurlburt <blakehur@buffalo.edu>
2018-09-19 15:04:13 -0400

    Fix links to use href parameter in Button

--- src/pages/dfa_home.js
+++ src/pages/dfa_home.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
@@ -8,19 +6,6 @@ import IconButton from '@material-ui/core/IconButton';
 import ArrowBack from '@material-ui/icons/ArrowBack';
 
 import { Link } from 'gatsby';
-
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
 
 class ButtonAppBar extends React.Component {
   render(){
@@ -31,14 +16,8 @@ class ButtonAppBar extends React.Component {
           <IconButton color="inherit" aria-label="Menu">
             <ArrowBack />
           </IconButton>
-
-          <Link to="/dfa_run">
-            <Button color="secondary">Edit</Button>
-          </Link>
-
-          <Link to="/dfa_run/">
-            <Button color="secondary">Run</Button>
-          </Link>
+          <Button color="inherit" href="/dfa_edit/">Edit</Button>
+          <Button color="inherit" href="/dfa_run/">Run</Button>
         </Toolbar>
       </AppBar>
     </div>
