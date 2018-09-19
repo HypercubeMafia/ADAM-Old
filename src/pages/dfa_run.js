d737c1ac2b5e48090d9de699376bd945538cfb63
Blake Hurlburt <blakehur@buffalo.edu>
2018-09-19 15:09:11 -0400

    Create Run page

---
+++ src/pages/dfa_run.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class editpage extends React.Component {
+  render(){
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="inherit" aria-label="Menu" href="/dfa_home/">
+            <ArrowBack />
+          </IconButton>
+          <Button color="inherit" href="/dfa_edit/">Edit</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+}
+
+export default editpage;
